a45e8da5cec8de2cd931ad513b63062fca50cb45
BreadTheSire <iisbread7@gmail.com>
2024-11-01 01:45:32 +0800

    feat: add aerospace and do some file updates

---
+++ macos/README.md
@@ -0,0 +1,7 @@
+# MacOS setup
+
+## Textfox
+
+Root directory may be subjected to changes
+
+Find root directory in firefox: `about:profiles` <- 'profile in use and it cannot be deleted'
--- unix/.config/wezterm/config/fonts.lua
+++ unix/.config/wezterm/config/fonts.lua
@@ -5,25 +5,24 @@ local default_font = "FantasqueSansM Nerd Font Propo"
 local default_font_size = platform().is_mac and 13 or 9
 
 return {
-  --font = wezterm.font(default_font),
-  --font_size = 14,
-  font_size = default_font_size,
-  font = wezterm.font_with_fallback {
-      default_font,
-      "JetBrainsMono Nerd Font",
-      "Iosevka Nerd Font",
-      "SauceCodePro Nerd Font Mono",
-      "FantasqueSansM Nerd Font",
-      "Inconsolata Nerd Font Mono",
-    -----------------------------------
-      "ZedMono Nerd Font Mono",
-      "SpaceMono Nerd Font",
-      "0xProto Nerd Font Mono",
-      "iMWritingMono Nerd Font Mono",
-  },
+	--font = wezterm.font(default_font),
+	-- font_size = 14,
+	font_size = default_font_size,
+	font = wezterm.font_with_fallback({
+		default_font,
+		"JetBrainsMono Nerd Font",
+		"Iosevka Nerd Font",
+		"SauceCodePro Nerd Font Mono",
+		"FantasqueSansM Nerd Font",
+		"Inconsolata Nerd Font Mono",
+		-----------------------------------
+		"ZedMono Nerd Font Mono",
+		"SpaceMono Nerd Font",
+		"0xProto Nerd Font Mono",
+		"iMWritingMono Nerd Font Mono",
+	}),
 
-  -- https://wezfurlong.org/wezterm/config/lua/config/freetype_load_target.html
-  freetype_load_target = 'Normal', ---@type 'Normal'|'Light'|'Mono'
-  freetype_render_target = 'Normal', ---@type 'Normal'|'Light'|'Mono'
+	-- https://wezfurlong.org/wezterm/config/lua/config/freetype_load_target.html
+	freetype_load_target = "Normal", ---@type 'Normal'|'Light'|'Mono'
+	freetype_render_target = "Normal", ---@type 'Normal'|'Light'|'Mono'
 }
-
