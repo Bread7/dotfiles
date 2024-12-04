b6cf0472507766ebadadbedafc6297a6607535e7
BreadTheSire <iisbread7@gmail.com>
2024-12-04 17:02:53 +0800

    fix: java gui issues on wayland with opengl

--- parrot-scripts/force-opengl.sh
+++ parrot-scripts/force-opengl.sh
@@ -3,6 +3,8 @@
 echo "Force OpenGL acceleration (VM only)"
 echo "This sets environment to occur on boot, before login"
 echo "see: https://github.com/labwc/labwc/issues/1829"
+echo "see: https://forum.portswigger.net/thread/missing-gui-elements-with-ubuntu-22-04-wayland-565abbae"
 
-echo "LIBDL_ALWAYS_SOFTWARE=1" | sudo tee -a /etc/environment
+echo "LIBGL_ALWAYS_SOFTWARE=1" | sudo tee -a /etc/environment
+echo "XWAYLAND_NO_GLAMOR=1" | sudo tee -a /etc/environment
 echo -e "\n"
